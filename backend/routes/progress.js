70c31ba220a6fda50e1f4154a18aad13c6d3a3a7
KushagraGaur <kushagragaur31@gmail.com>
2025-05-29 01:19:15 +0530

    deletion

--- backend/routes/progress.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const progressController = require('../controllers/progressController');
-
-router.get('/:userId', progressController.getUserProgress); 
-router.post('/:userId', progressController.updateProgress);
-
-module.exports = router;
